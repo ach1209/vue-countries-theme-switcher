2bb2841a2ddb6b878c4327c9d018432177cdf52b
Anthony <ach0074@gmail.com>
2023-02-24 22:17:44 -0500

    updated config to include base url for deployment

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vue-countries-theme-switcher/',
   plugins: [vue()],
   resolve: {
     alias: {
